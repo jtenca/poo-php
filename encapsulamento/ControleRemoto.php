ef50538323ace97d3e3f8174707649b3cfbd9c70
jtenca <developertenca@outlook.com>
2020-03-30 15:15:02 -0300

    finalização da implementação da interface

--- encapsulamento/ControleRemoto.php
+++ encapsulamento/ControleRemoto.php
@@ -39,43 +39,66 @@ class ControleRemoto  implements Controlador {
     }
 
     public function ligar() {
+        $this->setLigado(true);
 
     }
 
     public function desligar() {
+        $this->setLigado(false);
 
     }
 
     public function abrirMenu() {
+        echo "<br>Ligado?: " . ($this->getLigado()?"SIM":"NÃO");
+        echo "<br>Tocando?:" . ($this->getTocando()?"SIM":"NÃO");
+        echo "<br>Volume: " . $this->getVolume();
+
+        for ($i=0; $i <= $this->getVolume(); $i+= 10) {
+            echo "|";
+        }
+
+        echo "<br>";
 
     }
 
     public function fecharMenu() {
-
+        echo "<br> Fechando Menu";
     }
 
     public function maisVolume() {
-
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume() + 5);
+        }
     }
 
     public function menosVolume() {
-
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume() - 5);
+        }
     }
 
     public function ligarMudo() {
-
+        if ($this->getLigado() && $this->getVolume() > 0) {
+            $this->setVolume(0);
+        }
     }
 
     public function desligarMudo() {
-
+        if ($this->getLigado() && $this->getVolume() == 0) {
+            $this->setVolume(50);
+        }
     }
 
     public function play() {
-
+        if ($this->getLigado() && !($this->getTocando())) {
+            $this->setTocando(true);
+        }
     }
 
     public function pause() {
-        
+        if ($this->getLigado() && !($this->getTocando())) {
+            $this->setTocando(false);
+        }
     }
 
 }
